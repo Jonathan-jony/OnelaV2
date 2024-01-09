ccc5a1196a2e9ba4f28052480dffdabc7184da53
Jonathan-jony <79204604+Jonathan-jony@users.noreply.github.com>
2024-01-09 11:50:15 +0100

    add new version

--- docs/version 1.2/ScriptInsertionBDD.sql
+++ docs/version 1.2/ScriptInsertionBDD.sql
@@ -1,7 +1,7 @@
-INSERT INTO contact (firstname, lastname, phone_number, active) VALUES ("jony", "mujanovic", "0782587881", "1");
-INSERT INTO contact (firstname, lastname, phone_number, active) VALUES ("Ahmed", "mujanovic", "0772587881", "1");
-INSERT INTO contact (firstname, lastname, phone_number, active) VALUES ("Adem", "mujanovic", "0792587881", "1");
-INSERT INTO contact (firstname, lastname, phone_number, active) VALUES ("Selma", "mujanovic", "0762587881", "1");
+INSERT INTO contact (firstname, lastname, phone_number, ACTIVE, image) VALUES ("jony", "mujanovic", "0782587881", "1", "bonhomme");
+INSERT INTO contact (firstname, lastname, phone_number, ACTIVE, image ) VALUES ("Ahmed", "mujanovic", "0772587881", "1", "bonhomme");
+INSERT INTO contact (firstname, lastname, phone_number, ACTIVE, image) VALUES ("Adem", "mujanovic", "0792587881", "1", "bonhomme");
+INSERT INTO contact (firstname, lastname, phone_number, active, image) VALUES ("Selma", "mujanovic", "0762587881", "1", "bonhomme");
 
 INSERT INTO `group` (`name`) VALUES ('Onela');
 
